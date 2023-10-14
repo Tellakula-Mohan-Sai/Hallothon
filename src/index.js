cd1ddf8554ed71ff538d3494ad5da465708dc50f
Karrtt <karthik.m1705@gmail.com>
2023-10-14 11:42:43 +0530

    added login auth

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+import UserHome from './UserHome';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/userhome" element={<UserHome/>} />
+    </Routes>
+  </Router>,
+  document.getElementById('root')
 );
-
